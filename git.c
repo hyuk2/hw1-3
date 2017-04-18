7803d3b3a0669ef0e42416e6148939c3799b09d4
JunHyuk Kim <hyuk9292@naver.com>
2017-04-18 23:17:29 +0900

    Commit 4

--- git.c
+++ git.c
@@ -3,6 +3,7 @@ public Git{
     System.out.println("첫번째 커밋하기 전!");
     System.out.println("첫번째 커밋 완료 , 두번째 커밋하기 전입니다.");
     System.out.println("두번째 커밋 완료 , 세번째 커밋하기 전입니다.");
+    System.out.println("네번째 커밋하기 전입니다.");
 
     }
 }
